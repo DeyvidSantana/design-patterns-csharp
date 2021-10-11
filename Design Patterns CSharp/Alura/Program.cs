2caf30be62bd748ba0f7d9714f13ca8099610893
Deyvid Santana <sdsdeyvidh@outlook.com>
2021-10-10 21:28:59 -0300

    Criado projeto com exemplos de padrões de projetos ensinados no curso da Alura.

---
+++ Design Patterns CSharp/Alura/Program.cs
@@ -0,0 +1,28 @@
+﻿using Alura.ChainOfResponsability;
+using System;
+
+namespace Alura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExecuteChainOfResponsability();
+        }
+
+        private static void ExecuteChainOfResponsability()
+        {
+            IDesconto d1 = new DescontoPorCincoItens();
+            IDesconto d2 = new DescontoPorMaisDeQuinhentosReais();
+            IDesconto d3 = new SemDesconto();
+
+            d1.Proximo = d2;
+            d2.Proximo = d3;
+
+            Orcamento orcamento = new Orcamento(500.0);
+
+            double desconto = d1.Desconta(orcamento);
+            Console.WriteLine(desconto);
+        }
+    }
+}
